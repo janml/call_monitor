2a6acdefaa51cb5b7b2210f9c48292804be0f937
janml <jan-michael.loehlein@gmx.de>
2025-05-04 14:16:38 +0200

    Include all settings in the secrets example.

--- include/secrets_example.h
+++ include/secrets_example.h
@@ -1,2 +1,6 @@
-#define WIFI_SSID "TODO: Add wifi ssid here!"
-#define WIFI_PASSWORD "TODO: Add wifi password here!"
+#define WIFI_SSID "TODO: Add your wifi!"
+#define WIFI_PASSWORD "TODO: Add your wifi password!"
+#define TR064_PORT 49000
+#define TR064_HOST "fritz.box"
+#define TR064_USER "TODO: Add your tr064 username here!"
+#define TR064_PASSWORD "TODO: Add your tr064 password here!"
