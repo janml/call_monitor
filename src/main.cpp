259b0cee9ec00b5efb39e33d22cb336e0cb9cde4
janml <jan-michael.loehlein@gmx.de>
2025-05-10 19:10:05 +0200

    Add functioning prototype.

--- src/main.cpp
+++ src/main.cpp
@@ -9,26 +9,28 @@
 #include "secrets.h"
 
 
-const uint MAX_CALLS_TO_DISPLAY = 8;  // IMPROVEMENT: Move to settings?
+const uint MAX_CALLS_TO_DISPLAY = 10;  // IMPROVEMENT: Move to settings?
 
 const uint PAST_DAYS_TO_REQUEST_CALLS_FOR = 2;
 
 
 const int CALL_MISSED = 1;
 const int CALL_INCOMMING = 2;
-const int CALL_OUTGOING = 2;
+const int CALL_OUTGOING = 3;
 
 struct Call {
   int type;
   String call_datetime;
   String caller_phone_number;
+  String called_phone_number;
 };
 
-
+uint how_many_available_calls = 0;
+uint currently_displayed_call = 0;
 Call calls[MAX_CALLS_TO_DISPLAY];
 
 
-void establish_and_wait_for_wifi_connection() {
+void setup_wifi_connection() {
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   while (WiFi.status() != WL_CONNECTED) {
     delay(100);
@@ -105,29 +107,33 @@ String get_xml_tag_value(String s, String tag) {
   );
 }
 
-void parse_call_list(String xml) {
-  Serial.println("Parsing call list xml...");
+void parse_missed_or_incoming_calls(String call_list_xml) {
+  Serial.println("Parsing calls...");
 
+  uint call_add_index = 0;
   for (uint i = 0; i < MAX_CALLS_TO_DISPLAY; i++) {
-    String call_data = get_xml_tag(xml, "Call");
+    String call_xml_line = get_xml_tag(call_list_xml, "Call");
 
-    bool all_calls_are_parsed = !call_data.length();
+    bool all_calls_are_parsed = !call_xml_line.length();
     if (all_calls_are_parsed) {
       Serial.println("All calls are parsed");
       break;
     }
 
     Call call = {};
-    call.type = get_xml_tag_value(call_data, "Type").toInt();
-    call.call_datetime = get_xml_tag_value(call_data, "Date");
-    call.caller_phone_number = get_xml_tag_value(call_data, "Caller");
+    call.type = get_xml_tag_value(call_xml_line, "Type").toInt();
+    call.call_datetime = get_xml_tag_value(call_xml_line, "Date");
+    call.caller_phone_number = get_xml_tag_value(call_xml_line, "Caller");
+    call.called_phone_number = get_xml_tag_value(call_xml_line, "Called");
     
-    calls[i] = call;
+    calls[call_add_index] = call;
+    how_many_available_calls += 1;
+    call_add_index += 1;
 
-    // Remove processed xml line.
-    xml = remove_string(xml, call_data);
+    call_list_xml = remove_string(call_list_xml, call_xml_line);
   }
 }
+
 
 
 // REFACTOR: Move to toplevel declaration!
@@ -138,34 +144,102 @@ void parse_call_list(String xml) {
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
+
+void display_call() {
+  Call call = calls[currently_displayed_call];
+
+  String call_date = call.call_datetime.substring(0, 5);
+  String call_time = call.call_datetime.substring(9, 14);
+
+  String number;
+  String call_type_acronym;
+  if (call.type == CALL_MISSED) {
+    call_type_acronym = "Verpasst";
+    number = call.caller_phone_number;
+  } else if (call.type == CALL_INCOMMING) {
+    call_type_acronym = "Eingehend";
+    number = call.caller_phone_number;
+  } else if (call.type == CALL_OUTGOING) {
+    call_type_acronym = "Ausgehend";
+    number = call.called_phone_number;
+  }
+
+  display.clearDisplay();
+  display.setCursor(0, 14);
+  display.setTextColor(WHITE);
+  display.printf("%s %s", call_date, call_time);
+  display.setCursor(0, 35);
+  display.println(number);
+  display.setCursor(0, 56);
+  display.println(call_type_acronym);
+  display.display();
+}
+
 void setup() {
+  pinMode(4, INPUT_PULLUP);
+  pinMode(LED_BUILTIN, OUTPUT);
+
   Serial.begin(9600);
 
   if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
     Serial.println("SSD1306 allocation failed");
-    for(;;); // Don't proceed, loop forever
+    for (;;);
   }
 
   display.clearDisplay();
   display.setFont(&FreeSans9pt7b);
-  display.setCursor(0, 14);
   display.setTextColor(WHITE);
-  display.println("0/4 04.04 17:04");
-  display.setCursor(0, 35);
-  //display.setTextSize(2);
-  display.println("0800123456789");
+
+  display.clearDisplay();
+  display.setCursor(0, 14);
+  display.println("Verbinde mit Wlan");
   display.display();
 
-  establish_and_wait_for_wifi_connection();
+  setup_wifi_connection();
   
+  display.clearDisplay();
+  display.setCursor(0, 14);
+  display.println("Anrufe werden geladen");
+  display.display();
+
   String call_list_xml = request_call_list_xml();
   if (!call_list_xml.length()) {
-    Serial.println("Failed to get calls xml!");
+    display.clearDisplay();
+    display.setCursor(0, 14);
+    display.println("Fehler beim laden der Anrufe!");
+    display.display();
     return;
   }
-  parse_call_list(call_list_xml);
-  Serial.println("Calls retrieved.");
+
+  Serial.println(call_list_xml);
+
+  parse_missed_or_incoming_calls(call_list_xml);
+
+  Serial.printf("%i calls available\n", how_many_available_calls);
+
+  currently_displayed_call = 0;
+  display_call();
 }
 
+
+bool button_was_pressed = false;
+bool is_buildin_led_turned_on = false;
+
+
 void loop() {
+  int button_state = digitalRead(4);
+
+  if (button_state == LOW) {
+    button_was_pressed = true;
+  } else if (button_state == HIGH && button_was_pressed) {
+    button_was_pressed = false;
+
+    currently_displayed_call += 1;
+    if (currently_displayed_call >= how_many_available_calls) {
+      currently_displayed_call = 0;
+    }
+    display_call();
+  }
+
+  delay(50);
 }
